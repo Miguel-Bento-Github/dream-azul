bf858e912dbadebebe17ae73a0a5b9d38115bf7f
b3nt0 <document.body.title@gmail.com>
2024-12-21 19:43:35 +0100

    update version cache path

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import { router } from './router'
-import { versionCachePlugin } from './service/version-Cache'
+import { versionCachePlugin } from './service/version-cache'
 
 const app = createApp(App)
 
